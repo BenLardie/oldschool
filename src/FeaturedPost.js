11104d75a0e1ba6d57f589db71231626e2e58dbb
Ben Lardie <benlardie@gmail.com>
2020-03-03 15:47:56 -0500

    created new component and added material ui

---
+++ src/FeaturedPost.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './FeaturedPost.css';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+
+export default function FeaturedPost() {
+    return (
+        <div className="FeaturedPost">
+            <Paper>
+                <Typography variant="headline" component="h3">
+                    Cake
+                </Typography>
+            <div className="authorName">
+                <p>Giorgio Vasari</p>
+            </div>
+                <div className="content">
+                I am the first great appreciator of art, and I also appreciate the powerful sound of loud trumpets and references to driving cars.
+                </div>
+                    <button className="playArtist">Play Discography</button>
+            </Paper>
+        </div>
+    )
+}
